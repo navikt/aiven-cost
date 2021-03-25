e46f1e41c199ef37901a2ddacd6ed6723e7b0ee1
Truls Jørgensen <trulsjor@gmail.com>
2021-03-25 11:43:44 +0100

    Removed when clause in Aiven.kt

--- src/main/kotlin/aiven/cost/Aiven.kt
+++ src/main/kotlin/aiven/cost/Aiven.kt
@@ -11,37 +11,28 @@ class Aiven(val token: String, val hostAndPort: String = "https://api.aiven.io")
 
     private val client = OkHttpClient.Builder().callTimeout(5, TimeUnit.SECONDS).build()
 
-    fun getInvoiceData(): Map<String, List<InvoiceLine>?> {
-        val billingGroupdId = getBillingGroup()
+    fun getInvoiceData(): Map<String, List<InvoiceLine>> =
+        getBillingGroup()?.let {
+            getInvoices(it)?.let { invoices -> toInvoiceMap(invoices, it) }
+        }.orEmpty()
 
-        val invoices = when {
-            billingGroupdId.isEmpty() -> emptyList()
-            else -> getInvoices(billingGroupdId)
-        }
-        return toInvoiceMap(invoices, billingGroupdId)
-    }
+    private fun toInvoiceMap(invoices: List<String>, billingGroupdId: String): Map<String, List<InvoiceLine>> =
+        invoices.map { invoice_id -> invoice_id to getInvoiceLines(billingGroupdId, invoice_id) }.toMap()
 
-    private fun toInvoiceMap(invoices: List<String>, billingGroupdId: String): Map<String, List<InvoiceLine>?> {
-        return invoices.map { invoice_id -> invoice_id to getInvoiceLines(billingGroupdId, invoice_id) }.toMap()
-    }
-
-    private fun getInvoiceLines(
-        billingGroupdId: String,
-        invoice_id: String
-    ): List<InvoiceLine>? {
+    private fun getInvoiceLines(billingGroupdId: String, invoice_id: String): List<InvoiceLine> {
         val body = callAiven("/v1/billing-group/$billingGroupdId/invoice/$invoice_id/lines")
         val list = JsonPath.parse(body)?.read<List<Map<String, String>>>("$.lines[*]").orEmpty()
         return list.map { InvoiceLine(it) }.toList()
     }
 
-    private fun getInvoices(billingGroupdId: String): List<String> {
+    private fun getInvoices(billingGroupdId: String): List<String>? {
         val body = callAiven("/v1/billing-group/$billingGroupdId/invoice")
-        return JsonPath.parse(body)?.read<List<String>>("$.invoices[*].invoice_number").orEmpty()
+        return JsonPath.parse(body)?.read("$.invoices[*].invoice_number")
     }
 
-    private fun getBillingGroup(): String {
+    private fun getBillingGroup(): String? {
         val body = callAiven("/v1/billing-group")
-        return JsonPath.parse(body)?.read<String>("$.billing_groups[0].billing_group_id").orEmpty()
+        return JsonPath.parse(body)?.read("$.billing_groups[0].billing_group_id")
     }
 
 
