4eae44650c440870c8383f61386933287dfaa437
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-04-08 13:51:07 +0200

    delete data before insert
    Co-authored-by: Gøran Berntsen <goran.berntsen@nav.no>
    Co-authored-by: Truls Jørgensen <truls.jorgensen@nav.no>

--- src/main/kotlin/io/nais/cost/bigquery/BigQuery.kt
+++ src/main/kotlin/io/nais/cost/bigquery/BigQuery.kt
@@ -1,13 +1,14 @@
 package io.nais.cost.bigquery
 
-import com.google.cloud.bigquery.BigQueryOptions
-import com.google.cloud.bigquery.InsertAllRequest
-import com.google.cloud.bigquery.InsertAllResponse
-import com.google.cloud.bigquery.TableId
+import com.google.cloud.bigquery.*
 import io.nais.cost.CostItem
 import org.slf4j.LoggerFactory
 
 class BigQuery {
+    val table = "costitems"
+    val dataset = "aivencost"
+    val project = "nais-analyse-prod-2dcc"
+
 
     private companion object {
         private val log = LoggerFactory.getLogger(BigQuery::class.java)
@@ -16,17 +17,26 @@ class BigQuery {
     private val bigquery =
         BigQueryOptions.newBuilder()
             .setLocation("europe-north1")
-            .setProjectId("nais-analyse-prod-2dcc")
+            .setProjectId(project)
             .build().service
 
     fun write(costItems: List<CostItem>): InsertAllResponse {
-        val builder = InsertAllRequest.newBuilder(TableId.of("aivencost", "costitems"))
+        if (costItems.isNotEmpty()) {
+            deleteContentFromTable()
+        }
+        val builder = InsertAllRequest.newBuilder(TableId.of(dataset, table))
         costItems.forEach { builder.addRow(toRow(it)) }
 
         val response = bigquery.insertAll(builder.build())
         if (response.hasErrors()) response.insertErrors.entries.forEach { log.info("insertError: ${it.value}") }
 
         return response
+    }
+
+    private fun deleteContentFromTable() {
+        val query = "TRUNCATE TABLE $project.$dataset.$table"
+        val results = bigquery.query(QueryJobConfiguration.newBuilder(query).build())
+        results.iterateAll().forEach { log.info("insertError: $it") }
     }
 }
 
