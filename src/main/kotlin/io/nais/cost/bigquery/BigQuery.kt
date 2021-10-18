687e81f2f3c6536a890da686f258435d095b688f
Truls Jørgensen <trulsjor@gmail.com>
2021-10-18 13:47:05 +0200

    Forbedret logging for Biqueryerrors. Flyttet instansiering av service for å unngå at biqueryservice timer ut
    Co-authored-by: Gøran Berntsen <goran.berntsen@nav.no>

--- src/main/kotlin/io/nais/cost/bigquery/BigQuery.kt
+++ src/main/kotlin/io/nais/cost/bigquery/BigQuery.kt
@@ -1,6 +1,7 @@
 package io.nais.cost.bigquery
 
 import com.google.cloud.bigquery.*
+import io.nais.cost.BiqQueryErrorCounter
 import io.nais.cost.CostItem
 import org.slf4j.LoggerFactory
 import java.time.format.DateTimeFormatter
@@ -15,28 +16,35 @@ class BigQuery {
         private val log = LoggerFactory.getLogger(BigQuery::class.java)
     }
 
+    private val errorCounter = BiqQueryErrorCounter()
+
     private val bigquery =
         BigQueryOptions.newBuilder()
             .setLocation("europe-north1")
             .setProjectId(project)
-            .build().service
+            .build()
 
-    fun write(costItems: List<CostItem>): InsertAllResponse {
+    fun write(costItems: List<CostItem>) {
         if (costItems.isNotEmpty()) {
             deleteContentFromTable()
         }
         val builder = InsertAllRequest.newBuilder(TableId.of(dataset, table))
         costItems.forEach { builder.addRow(toRow(it)) }
+        val request = builder.build()
 
-        val response = bigquery.insertAll(builder.build())
-        if (response.hasErrors()) response.insertErrors.entries.forEach { log.info("insertError: ${it.value}") }
-
-        return response
+        try {
+            val response = bigquery.service.insertAll(request)
+            if (response.hasErrors()) response.insertErrors.entries.forEach { log.error("insertError: ${it.value}") }
+        } catch (e: BigQueryException) {
+            errorCounter.countError()
+            log.error(e.message)
+            throw e
+        }
     }
 
     private fun deleteContentFromTable() {
         val query = "TRUNCATE TABLE `$project.$dataset.$table`"
-        val results = bigquery.query(QueryJobConfiguration.newBuilder(query).build())
+        val results = bigquery.service.query(QueryJobConfiguration.newBuilder(query).build())
         results.iterateAll().forEach { log.info("insertError: $it") }
     }
 }
