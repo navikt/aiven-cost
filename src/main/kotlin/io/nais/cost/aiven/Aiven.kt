43529227cd1887c0c6ab42c2b42124fd1c58ac4a
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2022-10-17 10:19:58 +0200

    doesnt work

--- src/main/kotlin/io/nais/cost/aiven/Aiven.kt
+++ src/main/kotlin/io/nais/cost/aiven/Aiven.kt
@@ -16,35 +16,45 @@ class Aiven(val token: String, val hostAndPort: String = "https://api.aiven.io")
 
     private val client = OkHttpClient.Builder().callTimeout(5, TimeUnit.SECONDS).build()
 
-    fun getInvoiceData(): List<InvoiceLine> =
-        getBillingGroup()?.let {
-            getInvoices(it)?.let { invoices -> flattenToInvoiceLines(invoices, it) }
-        }.orEmpty()
+    fun getInvoiceData(): List<InvoiceLine> {
+        var invoiceLines = mutableListOf<InvoiceLine>()
+        for (billingGroupId in getBillingGroups()) {
+            invoiceLines.addAll(flattenToInvoiceLines(getInvoices(billingGroupId), billingGroupId))
+        }
+        return invoiceLines
+    }
 
-    private fun flattenToInvoiceLines(invoices: List<String>, billingGroupdId: String): List<InvoiceLine> =
+    private fun flattenToInvoiceLines(
+        invoices: List<String>,
+        billingGroupdId: String,
+    ): List<InvoiceLine> =
         invoices.flatMap { invoiceId -> getInvoiceLines(billingGroupdId, invoiceId) }
 
     private fun getInvoiceLines(billingGroupdId: String, invoiceId: String): List<InvoiceLine> {
         val body = callAiven("/v1/billing-group/$billingGroupdId/invoice/$invoiceId/lines")
         val list = JsonPath.parse(body)?.read<List<Map<String, Any>>>("$.lines[*]").orEmpty()
-        val invoiceLines = list.map { InvoiceLine(invoiceId, it) }.toList()
+        val tenant = getTenant(billingGroupdId)
+        val invoiceLines = list.map { InvoiceLine(invoiceId, tenant, it) }.toList()
         invoiceLines.forEach { line -> log.info("Invoiceline ${line.invoiceId} to ${line.endTimestamp}") }
         return invoiceLines
     }
 
-    private fun getInvoices(billingGroupdId: String): List<String>? {
+    private fun getInvoices(billingGroupdId: String): List<String> {
         val body = callAiven("/v1/billing-group/$billingGroupdId/invoice")
         val invoices: List<String>? = JsonPath.parse(body)?.read("$.invoices[*].invoice_number")
         log.info("fetched invoices, got ${invoices?.size} ")
-        return invoices
+        return invoices.orEmpty()
     }
 
-    private fun getBillingGroup(): String? {
-        val body = callAiven("/v1/billing-group")
-        val billingGroupsIds:  List<String>?  = JsonPath.parse(body)?.read("$.billing_groups[*].billing_group_id")
-        return billingGroupsIds?.filter { id -> id.equals("7d14362d-1e2a-4864-b408-1cc631bc4fab") }?.first()
+    private fun getBillingGroups(): List<String> = callAiven("/v1/billing-group").getBillingGroupIds().orEmpty()
+
+    fun String.getBillingGroupIds(): List<String>? {
+        return JsonPath.parse(this)?.read("$.billing_groups[*].billing_group_id")
     }
 
+    fun getTenant(billingGroupdId: String): String {
+        return ""
+    }
 
     private fun callAiven(aivenApiUrl: String): String {
         return client.newCall(
--- src/main/kotlin/io/nais/cost/aiven/InvoiceLine.kt
+++ src/main/kotlin/io/nais/cost/aiven/InvoiceLine.kt
@@ -2,6 +2,7 @@ package io.nais.cost.aiven
 
 data class InvoiceLine(
     val invoiceId: String,
+    val tenant: String,
     private val input: Map<String, Any>
 ) {
     val projectName = "project_name".mapValueAsString()
@@ -11,6 +12,7 @@ data class InvoiceLine(
     val serviceType = "service_type".mapValueAsString()
     val beginTimestamp = "timestamp_begin".mapValueAsString()
     val endTimestamp = "timestamp_end".mapValueAsString()
+    val tenant = tenant
 
     private fun String.mapValueAsString() =
         if (input.containsKey(this)) input[this] as String else ""
